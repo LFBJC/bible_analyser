8d07f89b962bc34e207165f8b65fa0cbc28bcdaf
= <=>
2019-12-22 17:02:36 -0300

    versao final

--- analyser.py
+++ analyser.py
@@ -1,26 +1,93 @@
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
 import os
+import math
+from prompt_toolkit import print_formatted_text, HTML
+from prompt_toolkit.styles import Style
+from sklearn.decomposition import FastICA
+import matplotlib.pyplot as plt
+import re
+def normalize (matrix):
+	center = matrix.mean(0);
+	distances = np.array([np.linalg.norm(r-center) for r in matrix])
+	mean_dist = distances.mean()
+	matrix = [(r-center)/mean_dist for r in matrix]
+	return matrix
+name_by_abbreviation = {
+	'gn': 'Genesis','ex': 'Exodo', 'lv': 'Levitico', 'nm': 'Numeros', 'dt':'Deuteronomio',
+	'js': 'Josue', 'jz': 'Juizes', 'rt': 'Rute', '1sm': '1 Samuel', '2sm': '2 Samuel',
+	'1rs': '1 Reis', '2rs': '2 Reis', '1cr': '1 Cronicas', '2cr': '2 Cronicas', 'ed': 'Esdras',
+	'ne': 'Neemias', 'et': 'Ester', 'jó': 'Jó', 'sl': 'Salmos', 'pv': 'Provérbios',
+	'ec': 'Eclesiastes','ct': 'Cantares','is': 'Isaias', 'jr': 'Jeremias', 'lm': 'Lamentações',
+	'ez': 'Ezequiel', 'dn': 'Daniel','os': 'Oseias', 'jl': 'Joel', 'am': 'Amos', 'ob': 'Obadias',
+	'jn': 'Jonas', 'mq': 'Miqueias', 'na': 'Naum', 'hc': 'Habacuque', 'sf': 'Sofonias', 'ag': 'Ageu',
+	'zc': 'Zacarias', 'ml': 'Malaquias', 'mt': 'Mateus', 'mc': 'Marcos', 'lc': 'Lucas', 'jo': 'Joao',
+	'atos': 'Atos', 'rm': 'Romanos', '1co': '1 Corintios', '2co': '2 Corintios', 'gl': 'Galatas',
+	'ef': 'Efesios', 'fp': 'Filipenses', 'cl': 'Colosenses', '1ts': '1 Tessalonisenses',
+	'2ts': '2 Tessalonisenses', '1tm': '1 Timoteo', '2tm': '2 Timoteo', 'tt': 'Tito',
+	'fm': 'Filemom', 'hb': 'Hebreus', 'tg': 'Tiago', '1pe': '1 Pedro', '2pe': '2 Pedro',
+	'1jo': '1 Joao', '2jo': '2 Joao', '3jo': '3 Joao', 'jd': 'Judas', 'ap': 'Apocalipse'
+}
 books_texts = {};
 for path in os.listdir('by_book'):
 	f = open('by_book\\'+path, 'r', encoding='utf-8');
-	books_texts[path] = f.read();
+	books_texts[path.replace('.txt','')] = f.read();
 	f.close();
 chapters_texts = {};
 for path in os.listdir('by_chapter'):
 	f = open('by_chapter\\'+path, 'r', encoding='utf-8');
 	chapters_texts[path] = f.read();
 	f.close();
-# list of text documents
+# list of text documents (input for vectorizer)
 full_text = [v for v in books_texts.values()]
 # create the transform
-vectorizer = CountVectorizer()
+vectorizer = TfidfVectorizer()
 # tokenize and build vocab
-vectorizer.fit(full_text)
+mat1 = vectorizer.fit_transform(full_text)
+mat2 = vectorizer.transform([v for v in chapters_texts.values()])
 # summarize
-print(vectorizer.vocabulary_)
-# encode documents
-#vector = vectorizer.transform(text)
-# summarize encoded vector
-#print(vector.shape)
-#print(type(vector))
-#print(vector.toarray())
+#print(vectorizer.vocabulary_)
+colors = {
+	'gn': '#FF0000','ex': '#7f4040', 'lv': '#4c2b26', 'nm': '#594643', 'dt':'#401100',
+	'js': '#662e1a', 'jz': '#7f5140', 'rt': '#8c7369', '1sm': '#993d00', '2sm': '#4c2a13',
+	'1rs': '#593000', '2rs': '#99754d', '1cr': '#734d00', '2cr': '#4c3913', 'ed': '#595243',
+	'ne': '#4B0082', 'et': '#998a4d', 'jó': '#4c4a26', 'sl': '#535900', 'pv': '#7d8060',
+	'ec': '#FF8C00','ct': '#305900','is': '#384030', 'jr': '#3e592d', 'lm': '#598c46',
+	'ez': '#608068', 'dn': '#005924','os': '#009952', 'jl': '#134d32', 'am': '#269982',
+	'ob': '#2d5956', 'jn': '#268299', 'mq': '#607980', 'na': '#005580', 'hc': '#2d4a59',
+	'sf': '#13324d', 'ag': '#003380', 'zc': '#234d8c', 'ml': '#606c80', 'mt': '#131b4d',
+	'mc': '#0000FF', 'lc': '#000040', 'jo': '#332d59', 'atos': '#464359', 'rm': '#290066',
+	'1co': '#4d238c', '2co': '#6c468c', 'gl': '#301040', 'ef': '#756080', 'fp': '#7a0099',
+	'cl': '#520066', '1ts': '#802079', '2ts': '#660052', '1tm': '#592d50', '2tm': '#40303a',
+	'tt': '#992663', 'fm': '#4d1332', 'hb': '#997387', 'tg': '#8c4662', '1pe': '#990029',
+	'2pe': '#7f2039', '1jo': '#592d39', '2jo': '#66000e', '3jo': '#4c131b', 'jd': '#806064',
+	'ap': '#000000'
+}
+#mat = [v.toarray() for v in list(books_vectors.values())]
+dim_reducer = FastICA(n_components=2)
+#books_dots = normalize(dim_reducer.fit_transform(mat1.toarray()))
+books_dots = dim_reducer.fit_transform(mat1.toarray())
+plt.clf()
+for dot_i in range(len(books_dots)):
+	plt.plot(books_dots[dot_i][0],books_dots[dot_i][1], color=colors[list(books_texts.keys())[dot_i]], marker='o')
+	plt.text(books_dots[dot_i][0],books_dots[dot_i][1],name_by_abbreviation[list(books_texts.keys())[dot_i]])
+ax = plt.gca()
+# recompute the ax.dataLim
+ax.relim()
+# update ax.viewLim using the new dataLim
+ax.autoscale_view()
+plt.show()
+#chapters_dots = normalize(dim_reducer.transform(mat2.toarray()))pytho
+chapters_dots = dim_reducer.transform(mat2.toarray())
+plt.clf()
+for dot_i in range(len(chapters_dots)):
+	chapters_doc_name = list(chapters_texts.keys())[dot_i]
+	print(chapters_doc_name)
+	plt.plot(chapters_dots[dot_i][0],chapters_dots[dot_i][1], color=colors[chapters_doc_name[0:re.search("\d*.txt",chapters_doc_name).start()]], marker='o')
+	plt.text(chapters_dots[dot_i][0],chapters_dots[dot_i][1],name_by_abbreviation[chapters_doc_name[0:re.search("\d*.txt",chapters_doc_name).start()]]+(list(chapters_texts.keys())[dot_i][2:].replace('.txt','')))
+ax = plt.gca()
+# recompute the ax.dataLim
+ax.relim()
+# update ax.viewLim using the new dataLim
+ax.autoscale_view()
+plt.show()
--- writer.py
+++ writer.py
@@ -1,13 +1,16 @@
+# -*- coding: utf-8 -*-
 import os
 import time
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 from bs4.element import Comment
-url = 'https://www.bible.com/pt/bible/';
-versions_vt = [(904, 'WLC'), (129, 'NVI'), (212, 'ARC')];
-versions_nt = [(183, 'TR1894'), (129, 'NVI'), (212, 'ARC')];
-books_vt = ['GEN', 'EXO', 'LEV', 'NUM', 'DEU', 'JOS', 'JDG', 'RUT', '1SA', '2SA', '1KI', '2KI', '1CH', '2CH', 'EZR', 'NEH', 'EST', 'JOB', 'PSA', 'PRO', 'ECC', 'SNG', 'ISA', 'JER', 'LAM', 'EZK', 'DAN', 'HOS', 'JOL', 'AMO', 'OBA', 'JON', 'MIC', 'NAM', 'HAB', 'ZEP', 'HAG', 'ZEC', 'MAL']
-books_nt = ['MAT', 'MRK', 'LUK', 'JHN', 'ACT', 'ROM', '1CO', '2CO', 'GAL', 'EPH', 'PHP', 'COL', '1TH', '2TH', '1TI', '2TI', 'TIT', 'PHM', 'HEB', 'JAS', '1PE', '2PE', '1JN', '2JN', '3JN', 'JUD', 'REV'];
+from urllib.parse import quote
+url = 'https://www.bibliaonline.com.br/';
+#os idiomas originais tinham separado muito o antigo e o novo testamento
+versions_vt = ['nvi','acf'];#(904, 'WLC') - hebraico
+versions_nt = ['nvi','acf'];#(183, 'TR1894') - grego
+books_vt = ['gn', 'ex', 'lv', 'nm', 'dt', 'js', 'jz', 'rt', '1sm', '2sm', '1rs', '2rs', '1cr', '2cr', 'ed', 'ne', 'et', 'jó', 'sl', 'pv', 'ec', 'ct', 'is', 'jr', 'lm', 'ez', 'dn', 'os', 'jl', 'am', 'ob', 'jn', 'mq', 'na', 'hc', 'sf', 'ag', 'zc', 'ml']
+books_nt = ['mt', 'mc', 'lc', 'jo', 'atos', 'rm', '1co', '2co', 'gl', 'ef', 'fp', 'cl', '1ts', '2ts', '1tm', '2tm', 'tt', 'fm', 'hb', 'tg', '1pe', '2pe', '1jo', '2jo', '3jo', 'jd', 'ap'];
 chapters_vt = [50, 40, 27, 36, 34, 24, 21, 4, 31, 24, 22, 25, 29, 36, 10, 13, 10, 42, 150, 31, 12, 8, 66, 52, 5, 48, 12, 14, 3, 9, 1, 4, 7, 3, 3, 3, 2, 14, 4]
 chapters_nt = [28, 16, 24, 21, 28, 16, 16, 13, 6, 6, 4, 4, 5, 3, 6, 4, 3, 1, 13, 5, 5, 3, 5, 1, 1, 1, 22];
 def tag_visible(element):
@@ -25,14 +28,14 @@ def get_encoding(soup):
 	return encod
 os.mkdir('by_chapter/');
 os.mkdir('by_book/')
- for i in range(len(books_vt)):
+for i in range(len(books_vt)):
  	book_string = u'';
  	f1 = open('by_book/'+books_vt[i]+'.txt','w+',encoding='utf-8');
  	for j in range(1,chapters_vt[i]+1):
  		f2 = open('by_chapter/'+books_vt[i]+str(j)+'.txt','w+',encoding='utf-8');
  		str_to_be_written = u'';
  		for v in versions_vt:
- 			get_url = url + str(v[0]) + '/' + books_vt[i] + '.' + str(j) + '.' + str(v[1]);
+ 			get_url = url + v + '/' + quote(books_vt[i]) + '/' + str(j);
  			req = Request(get_url, headers={'User-Agent': 'Mozilla/71.0'});
  			webpage = urlopen(req).read();
  			print('Funcionou: ', get_url);
@@ -40,7 +43,7 @@ os.mkdir('by_book/')
  			visible_texts = filter(tag_visible, sopa.findAll(text=True));
  			text = u" ".join(t.strip() for t in visible_texts)
  			str_to_be_written += text;
- 			time.sleep(1);
+ 		time.sleep(2);
  		f2.write(str_to_be_written);
  		book_string += str_to_be_written;
  		f2.close();
@@ -53,7 +56,7 @@ for i in range(len(books_nt)):
 		f2 = open('by_chapter/'+books_nt[i]+str(j)+'.txt','w+',encoding='utf-8');
 		str_to_be_written = u'';
 		for v in versions_nt:
-			get_url = url + str(v[0]) + '/' + books_nt[i] + '.' + str(j) + '.' + str(v[1]);
+			get_url = url + v + '/' + quote(books_nt[i]) + '/' + str(j);
 			req = Request(get_url, headers={'User-Agent': 'Mozilla/71.0'});
 			webpage = urlopen(req).read();
 			print('Funcionou: ', get_url);
@@ -61,7 +64,7 @@ for i in range(len(books_nt)):
 			visible_texts = filter(tag_visible, sopa.findAll(text=True));
 			text = u" ".join(t.strip() for t in visible_texts)
 			str_to_be_written += text;
-			time.sleep(1);
+		time.sleep(2);
 		f2.write(str_to_be_written);
 		book_string += str_to_be_written;
 		f2.close();
